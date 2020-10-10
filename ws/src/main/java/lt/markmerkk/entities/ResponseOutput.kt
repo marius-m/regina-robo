69837872a1a6a99591753a0036023ad8ddc29c76
Marius Merkevicius <merkevicius.marius@gmail.com>
2020-10-10 22:15:29 +0300

    Records duration

--- ws/src/main/java/lt/markmerkk/entities/ResponseOutput.kt
+++ ws/src/main/java/lt/markmerkk/entities/ResponseOutput.kt
@@ -3,5 +3,6 @@ package lt.markmerkk.entities
 data class ResponseOutput(
         val id: String,
         val text: String,
+        val recordDurationMillis: Long,
         val resources: List<String>
 )
